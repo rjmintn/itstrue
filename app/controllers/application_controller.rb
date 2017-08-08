9d810fa9386d70a2ad2ecdfe82197e287faf38ab
rjmintn <rjmorawski@gmail.com>
2017-08-08 16:43:51 -0400

    Partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
