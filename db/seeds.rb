5360a8643b5e74bc5f05d2d89a0f333267285cd0
rjmintn <rjmorawski@gmail.com>
2017-08-09 17:36:21 -0400

    user story 5 seeding complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.all
+
+25.times do |count|
+  wiki = Wiki.create!(
+    title: "#{count.to_s} " + Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph(2,false,4),
+    private: Faker::Boolean.boolean,
+    user_id: users.sample
+  )
+  wiki.update_attributes(:created_at => Faker::Date.between(12.months.ago, 6.months.ago),  :updated_at => Faker::Date.between(5.months.ago, Date.today))
+end
