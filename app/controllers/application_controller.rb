2eade47e85a5ac6a320dd90d517256ac0a27e9d1
rjmintn <rjmorawski@gmail.com>
2017-08-09 12:41:58 -0400

    adjust devise to allow create without email verify until sendgrid fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_user!
-  #after_initialize :after_init
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def after_init
-    return unless new_record?
-    self.status = ACTIVE
-  end
 end
